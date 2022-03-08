07c7a2a1598e446e92e898ef5a12ad406f48cc0a
sharanchauhan <sharanchauhan29@gmail.com>
2022-03-08 22:24:58 +0530

    Complex objects with keys and value as String,Number,Array,Object and Funciton

--- Module-2/object.js
+++ Module-2/object.js
@@ -1,10 +1,26 @@
+// Empty Object
 let a={}
-console.log(a);
+console.log(a);//Printing the empty object
 
 let obj1={
-    "First Name":"Sharan",
-    "Last Name":"Chauhan",
-    Phone_No: 7042761320,
-    Age:'21'
+    Name:"Sharan Chauhan",
+    phoneNo: 7042761320,
+    Age:21,
+    lastName:'Chauhan'
 };
 console.log(obj1);
+
+let captAmerica={
+    Name:'Steve',
+    Age:150,
+    Friends:["Natasha","Tony","Thor","Bruce","Vision"],
+    address:{
+        city:"Queens",
+        state:"Washington"
+    },
+    sayHi:function(){
+        console.log("Captain America Says Hi !!");
+    }
+}
+
+console.log(captAmerica);
