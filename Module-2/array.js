23961c9e2e48c1ecb353dcebdd6dfcaec498a814
sharanchauhan <sharanchauhan29@gmail.com>
2022-03-08 21:34:40 +0530

    Changed the array name to eleArr which was before arr

--- Module-2/array.js
+++ Module-2/array.js
@@ -1,22 +1,27 @@
-// Declaring and printing an empty array
-let arr=[]
+//empty array
+let arr = [];
 console.log(arr);
 
-// Declaring and initializing the array with different types of data
-let a=[1,2,3,4,5,true,"Hello World!!",6.7,false]
-console.log(a);
+// array with elements
 
-console.log("Element at 0th index = "+a[0]);
-console.log("Element at 5th index = "+a[5]);
-console.log("Element at 6th index = "+a[6]);
-a[2]="Changed";
-console.log("**************************************************************************************");
-// Array methods:-
-// 1.push
+let eleArr = [1,2,4,5,"Hello I am String",false,'c',4.5]
+console.log(eleArr);
 
-console.log("Array before push = "+a);
-a.push("New Item");
-console.log("Array after push = "+a);
+console.log("Element at 4th index: " + eleArr[4]);
+
+console.log("Element at 0th index : "+ eleArr[0]);
+
+eleArr[3] = "Nothing";
+
+console.log(eleArr);
+
+console.log("###############################################")
+
+//Arrays method
+//1. push
+console.log("Array before push: ",eleArr);
+eleArr.push("new item");
+console.log("Array after push : ",eleArr);
 
 //2. pop
 
