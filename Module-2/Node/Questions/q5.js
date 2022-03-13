f2c43e18a152de2d75b96bfabc29187f27810035
sharanchauhan <sharanchauhan29@gmail.com>
2022-03-13 09:19:54 +0530

    q5 done using mv module

--- Module-2/Node/Questions/q5.js
+++ Module-2/Node/Questions/q5.js
@@ -1,1 +1,13 @@
 //move a file
+// npm install mv
+let mv=require('mv');
+let source="/Users/sharanchauhan/Documents/FJP6/Module-2/Node/Questions/folder-q1/text-file.txt";
+let destination="/Users/sharanchauhan/Documents/FJP6/Module-2/Node/Dummy Folder/text-file.txt";
+
+mv(source, destination, function(err) {
+    if (err) {
+       throw err
+   } else {
+       console.log("Successfully moved the file!");
+   }
+});
