506591729d9b1806695b23060525c2ec14a94d5e
sharanchauhan <sharanchauhan29@gmail.com>
2022-03-13 09:33:34 +0530

    q4 done

--- Module-2/Node/Questions/q4.js
+++ Module-2/Node/Questions/q4.js
@@ -1,1 +1,13 @@
 // read content of unorganised folder and make  an array which has extension name of each file
+let fs=require('fs');
+let path=require('path');
+
+let data=fs.readdirSync("/Users/sharanchauhan/Documents/FJP6/Module-2/Node/unorganised");
+console.log(data);
+let arr_ext=[];
+for(let i=0;i<data.length;i++)
+{
+    arr_ext.push(path.extname(data[i]));
+}
+
+console.log(arr_ext);
