1086ba739bec21116b3ae036740ea9efe9ed1494
sharanchauhan <sharanchauhan29@gmail.com>
2022-03-15 22:30:23 +0530

    q2 done and executed successfully with Organised Folder added

--- Module-2/Node/Questions/q2.js
+++ Module-2/Node/Questions/q2.js
@@ -1,22 +1,28 @@
 // given an array arr=[audio,video,image,software,documents,applications,other]
 //make folder for each element in the given array and inside each folder make 4 files of that type
-let arr=["audio","video","image","software","documents","applications","other"];
+let arr=['audio','video','image','software','documents','Applications','other'];
+let name=['abc','def','ghi','jkl'];
+let ext=['.mp3','.mp4','.jpg','.exe','.docx','.apk','.rar'];
 let fs=require('fs');
 let path=require('path');
 
+let organised=path.join(__dirname,"Organised");
+if(!fs.existsSync(organised))
+{
+    fs.mkdirSync(organised);
+}
+
 for(let i=0;i<arr.length;i++)
 {
-    let n=arr[i];
-    let folder=path.join(__dirname,n);
-    if(arr[i]==n)
+    let folder=path.join(organised,arr[i]);
+    if(!fs.existsSync(folder))
     {
-        if(!fs.existsSync(folder))
-        {
-            fs.mkdirSync(folder);
-        }
-        fs.writeFileSync(path.join(folder,"f1.mp3"),"");
-        fs.writeFileSync(path.join(folder,"f2.mp3"),"");
-        fs.writeFileSync(path.join(folder,"f3.mp3"),"");
-        fs.writeFileSync(path.join(folder,"f4.mp3"),"");
+        fs.mkdirSync(folder);
+    }
+    for(let j=0;j<name.length;j++)
+    {
+        let fileName=name[j]+ext[i];
+        let fileKaPath=path.join(folder,fileName);
+        fs.writeFileSync(fileKaPath,"");
     }
 }
