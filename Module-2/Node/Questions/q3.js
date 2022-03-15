e0ddf8c80008846e2e8f7e1d07105b3d212d1cc4
sharanchauhan <sharanchauhan29@gmail.com>
2022-03-15 22:50:49 +0530

    Modified q3.j and learnt the use of '..' in path

--- Module-2/Node/Questions/q3.js
+++ Module-2/Node/Questions/q3.js
@@ -1,7 +1,7 @@
 //copy index.html file from module1 to a new folder inside module2 having name html.
 let fs=require('fs');
 let path=require('path');
-let source="/Users/sharanchauhan/Documents/FJP6/Module-1/project/index.html";
-let destination=path.join("/Users/sharanchauhan/Documents/FJP6/Module-2/New Folder","html.html");
+let source=path.join(__dirname,"..","..","..","Module-1","project","index.html");
+let destination=path.join(__dirname,"..","..","html.html");
 
 fs.copyFileSync(source,destination);
