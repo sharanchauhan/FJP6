4a5ee600fab1946dd9164cd5eafc7ab1a1ae90dd
sharanchauhan <sharanchauhan29@gmail.com>
2022-03-17 22:05:50 +0530

    Checking if the path given is valid or not

--- Module-2/Project/fileOrganizer.js
+++ Module-2/Project/fileOrganizer.js
@@ -1,5 +1,19 @@
-let inputArr=process.argv;
+// let inputArr=process.argv;
 // console.log(inputArr);
 
-let input=inputArr[2];
-console.log(input);
+// let input=inputArr[2];
+let folder=process.argv[2];
+// console.log(input);
+
+let fs=require("fs");
+let folderExists=fs.existsSync(folder);
+if(folderExists)
+{
+    let files=fs.readdirSync(folder);
+    console.log(files);
+}
+else
+{
+    console.log("Please Enter a Valid Path");
+}
+
