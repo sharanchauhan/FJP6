549add74fa6f860e3666772c0d37703918fd7f39
sharanchauhan <sharanchauhan29@gmail.com>
2022-03-17 22:45:11 +0530

    Created a function which would check the type of file

--- Module-2/Project/fileOrganizer.js
+++ Module-2/Project/fileOrganizer.js
@@ -6,14 +6,44 @@ let folder=process.argv[2];
 // console.log(input);
 
 let fs=require("fs");
+let path=require("path");
 let folderExists=fs.existsSync(folder);
+
+let extensions={
+    Audio:[".mp3"],
+    Video:[".mp4",".mkv"],
+    Document:[".pdf",".xlsx",".doc",".txt"],
+    Image:[".jpg",".gif",".jpeg",".png"],
+    Software:[".exe"]
+}
+
 if(folderExists)
 {
     let files=fs.readdirSync(folder);
-    console.log(files);
+    for(let i=0;i<files.length;i++)
+    {
+        let ext=path.extname(files[i]);
+        let type=checkType(ext);
+        console.log(type);
+    }
 }
 else
 {
     console.log("Please Enter a Valid Path");
 }
 
+function checkType(ext)
+{
+    for(let key in extensions)
+    {
+        let extArr=extensions[key];
+        for(let i=0;i<extArr.length;i++)
+        {
+            if(ext==extArr[i])
+            {
+                return key;
+            }
+        }
+    }
+    return 'Others'
+}
