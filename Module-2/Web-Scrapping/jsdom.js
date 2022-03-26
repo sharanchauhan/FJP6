205c2ecb39f7b1896828394dacb63108a876dd9e
sharanchauhan <sharanchauhan29@gmail.com>
2022-03-26 23:50:43 +0530

    Getting the team names using jsdom tool

---
+++ Module-2/Web-Scrapping/jsdom.js
@@ -0,0 +1,20 @@
+
+const request = require('request');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const link = "https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/chennai-super-kings-vs-kolkata-knight-riders-1st-match-1304047/live-cricket-score";
+
+request(link, cb);
+
+function cb(error, response, html) {
+    if(error)
+        console.error('error:', error); // Print the error if one occurred
+    else{
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let teamsName = document.querySelectorAll(".match-info.match-info-MATCH.match-info-MATCH-half-width .teams .name");
+        console.log(teamsName[0].textContent);
+        console.log(teamsName[1].textContent);
+    }
+}
