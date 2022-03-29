9e75841a7963431458bd29f6103a3861095d181a
sharanchauhan <sharanchauhan29@gmail.com>
2022-03-29 22:40:32 +0530

    Activity 1 completed

--- Module-2/Web-Scrapping/highestWicketTaken.js
+++ Module-2/Web-Scrapping/highestWicketTaken.js
@@ -15,7 +15,28 @@ function cb(error,response,html)
         // console.log("HTML:-"+html);
         const dom = new JSDOM(html);
         const document = dom.window.document;
-        let table=document.querySelectorAll("table.bowler");
-        console.log(table.length);
+        let table=document.querySelectorAll(".table.bowler");
+        let highestWicket=0,taker="";
+        for(let i=0;i<table.length;i++)
+        {
+            let rows=table[i].querySelectorAll("tbody tr");
+            for(let j=0;j<rows.length;j++)
+            {
+                let data=rows[j].querySelectorAll("td");
+                if(data.length>1)
+                {
+                    let name=data[0].textContent;
+                    let wicket=data[4].textContent;
+                    // console.log("Bowler Name -> "+name);
+                    // console.log("Wickets     -> "+wicket);
+                    if(highestWicket<wicket)
+                    {
+                        highestWicket=wicket;
+                        taker=name;
+                    }
+                }
+            }
+        }
+        console.log(taker+" --> "+highestWicket);
     }
 }
