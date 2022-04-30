23e23fb5e900e3bb16bfb9085bb6cc91c973936e
sharanchauhan <sharanchauhan29@gmail.com>
2022-04-30 22:05:08 +0530

    modified script.js file

--- Module-3/todo/script.js
+++ Module-3/todo/script.js
@@ -1,15 +1,16 @@
-let inputTag = document.querySelector("input");
-let ulTag = document.querySelector("ul");
+let inputTag=document.querySelector('input');
+let ulTag=document.querySelector('ul');
 
-inputTag.addEventListener('keydown',function(e){
-    // console.log(e);
-    let key = e.key;
-    if(key == 'Enter'){
-        let value = inputTag.value;
-        // console.log(value);
-        inputTag.value = "";
-        let liTag = document.createElement("li");
-        liTag.innerText = value;
+inputTag.addEventListener('keydown',function(e)
+{
+    let key=e.key;
+    // The e object contains a value named key from which we are accessing the key pressed
+    if(key=='Enter')
+    {
+        let data=inputTag.value;
+        inputTag.value="";
+        let liTag=document.createElement('li');
+        liTag.innerText=data;
         ulTag.appendChild(liTag);
     }
 })
