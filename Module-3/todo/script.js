2ad2a94e04ddf62b655a4c098ca032bfe6228294
sharanchauhan <sharanchauhan29@gmail.com>
2022-05-02 20:52:23 +0530

    Formatted the document using Prettier and added a condition for emoty task

--- Module-3/todo/script.js
+++ Module-3/todo/script.js
@@ -1,29 +1,29 @@
-let inputTag=document.querySelector('input');
-let ulTag=document.querySelector('ul');
+let inputTag = document.querySelector("input");
+let ulTag = document.querySelector("ul");
 
-inputTag.addEventListener('keydown',function(e)
+inputTag.addEventListener("keydown",function(e) 
 {
-    let key=e.key;
-    // The e object contains a value named key from which we are accessing the key pressed
-    if(key=='Enter')
+  let key = e.key;
+  // The e object contains a value named key from which we are accessing the key pressed
+  if (key == "Enter") 
+  {
+    let data = inputTag.value;
+    if (data != "") 
     {
-        let data=inputTag.value;
-        inputTag.value="";
-        let liTag=document.createElement('li');
-        // liTag.innerText=data;
-        liTag.innerHTML=`<div>${data}</div>
-                        <div class="delete"><i class="fa fa-trash"></i></div>` // String interpolation
-        handelRemoval(liTag);
-        ulTag.appendChild(liTag);
+      inputTag.value = "";
+      let liTag = document.createElement("li");
+      // liTag.innerText=data;
+      liTag.innerHTML = `<div>${data}</div>
+                        <div class="delete"><i class="fa fa-trash"></i></div>`; // String interpolation
+      handelRemoval(liTag);
+      ulTag.appendChild(liTag);
     }
+  }
+});
 
-})
-
-function handelRemoval(liTag)
-{
-    let deleteDiv=liTag.querySelector('.delete');
-    deleteDiv.addEventListener("click",function()
-    {
-        liTag.remove();
-    });
+function handelRemoval(liTag) {
+  let deleteDiv = liTag.querySelector(".delete");
+  deleteDiv.addEventListener("click", function () {
+    liTag.remove();
+  });
 }
