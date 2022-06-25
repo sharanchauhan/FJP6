32a17fc0f8be35a103a2087c39b71faab39426f5
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-06-26 00:01:35 +0530

    Created todo-app with react command and removed unnecessary code and created folder named components in src folder and wrote todo.js script file with successfull export and import

---
+++ Module-4/React/todo-app/src/components/todo.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class Todo extends Component
+{
+    render(){
+        return(
+            <div>Hello!! Welcome to Sharan's World</div>
+        );
+    }
+}
+
+export default Todo;
