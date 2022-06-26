bc6bc33d83974638ecc5cc25f62646076eb0fc9d
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-06-26 22:25:27 +0530

    handleAddTask and handleChange functionality added with map and spread operator since we cannot use loops in react

--- Module-4/React/todo-app/src/components/todo.js
+++ Module-4/React/todo-app/src/components/todo.js
@@ -2,9 +2,41 @@ import { Component } from "react";
 
 class Todo extends Component
 {
+    constructor()
+    {
+        super();
+        this.state={
+            tasks:[],
+            currTask:""
+        }
+    }
+
+    handleChange=(e)=>{
+        this.setState({
+            currTask:e.target.value
+        })
+    }
+
+    handleAddTask=()=>{
+        this.setState({
+            tasks:[...this.state.tasks,this.state.currTask],
+            currTask:""
+        })
+    }
     render(){
         return(
-            <div>Hello!! Welcome to Sharan's World</div>
+            <div>
+                <input type="text" value={this.state.currTask} onChange={this.handleChange}></input>
+                <button onClick={this.handleAddTask}>Add Task</button>
+                <ul>
+                    {this.state.tasks.map((ele)=>(
+                        <li>
+                            <p>{ele}</p>
+                            <button>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         );
     }
 }
