49fa97fb66b01e67bdcf72f2b20825e2c0dcdbc7
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-07-05 23:07:58 +0530

    MovieList component added

--- Module-4/React/movies-app/src/App.js
+++ Module-4/React/movies-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
+import MovieList from './components/MovieList';
 function App() {
   return (
     <div>
       <NavBar/>
       <Banner/>
+      <MovieList/>
     </div>
   );
 }
