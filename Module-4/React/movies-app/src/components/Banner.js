88202d428b4b27d38478cc511529477bccc5e7f7
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-07-05 23:20:35 +0530

    MovieList Banner updated with movieData

--- Module-4/React/movies-app/src/components/Banner.js
+++ Module-4/React/movies-app/src/components/Banner.js
@@ -1,12 +1,20 @@
 import { Component } from "react";
+import {movies} from "../movieData"
+
+let movie = movies.results[0];
+let backdrop_path = movie.backdrop_path;
 
 class Banner extends Component {
   render() {
     return (
       <div className="card banner-card">
-        <img src="https://cdn.wallpapersafari.com/92/19/GT35xL.jpg" className="card-img-top banner-img" style={{height:700}} alt="..." />
-        <h1 class="banner-title">Captain America:Civil War</h1>
-        <p class="banner-text">Development of Civil War began in late 2013 when Markus and McFeely began writing the screenplay, which borrows concepts from the 2006 comic book storyline "Civil War" while also focusing on story and character elements from the previous Captain America films to conclude the trilogy. Following positive reactions to The Winter Soldier, the Russo brothers were brought back to direct in early 2014. The film's title and premise were revealed in October 2014, along with Downey's involvement as Stark; additional cast members joined in the following months.</p>
+        <img
+          src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
+          className="card-img-top banner-img"
+          alt="..."
+        />
+        <h1 className="card-title banner-title">{movie.title}</h1>
+        <p className="card-text banner-text">{movie.overview}</p>
       </div>
     );
   }
