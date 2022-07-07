488a8e1c2be10c08f641973d88ef7a8cd3abe387
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-07-07 22:23:03 +0530

    Removed irregullar elements from MovieList.js file in components folder in src

--- Module-4/React/movies-app/src/components/MovieList.js
+++ Module-4/React/movies-app/src/components/MovieList.js
@@ -54,21 +54,6 @@ class MovieList extends Component {
                   Previous
                 </a>
               </li>
-              <li className="page-item">
-                <a className="page-link" href="#">
-                  1
-                </a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">
-                  2
-                </a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">
-                  3
-                </a>
-              </li>
               {this.state.pArr.map((ele) => (
                 <li className="page-item">
                   <a className="page-link" href="#">
