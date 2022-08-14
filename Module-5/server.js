aec68ace7071c52fa86d6e9b468d523e61922cbd
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-14 12:00:32 +0530

    Learn't how to get values from the parameters in the url

--- Module-5/server.js
+++ Module-5/server.js
@@ -3,9 +3,22 @@ const express=require('express');
 //app signifies -> your server
 const app=express();
 
+app.get("/",function(req,res)
+{
+    res.send("Homepage");
+});
+
 app.get("/sayHello",function(req,res)
 {
     res.send("Hello!! From Server");
+});
+
+app.get("/getMultiply/:num1/:num2",function(req,res) {
+    console.log(req.params);
+    let num1=req.params.num1;
+    let num2=req.params.num2;
+    let result=num1*num2;
+    res.end("Multiplication of parameters = "+result);
 });
 
 app.get("/sayBye",function(req,res)
