0ce7da076788fb2e547d4298de6890dc9691fb10
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-14 19:11:16 +0530

    Learn't CRUD operations using post, get, patch, delete

--- Module-5/server.js
+++ Module-5/server.js
@@ -2,16 +2,51 @@ const express=require('express');
 
 //app signifies -> your server
 const app=express();
-
+app.use(express.json());
 app.get("/",function(req,res)
 {
     res.send("Homepage");
 });
 
+//get
 app.get("/sayHello",function(req,res)
 {
-    res.send("Hello!! From Server");
+    res.json({
+        user:user
+    })
 });
+
+//post
+app.post("/sayHello",function(req,res)
+{
+    user = req.body;
+    res.json({
+        message:"Data recieved Successfully",
+        user:user
+    })
+});
+
+//patch
+app.patch("/sayHello",function(req,res){
+    dataToUpdate = req.body;
+
+    for(key in dataToUpdate){
+        user[key] = dataToUpdate[key];
+    }
+    res.json({
+        message:"Data updated",
+        user:user
+    })
+})
+
+//delete
+app.delete("/sayHello",function(req,res){
+    user = {};
+    res.json({
+        message:"Deletion done",
+        user:user
+    })
+})
 
 app.get("/getMultiply/:num1/:num2",function(req,res) {
     console.log(req.params);
