1ce743db65aef4da4e88dbfca88abf31c565f14b
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-16 17:24:03 +0530

    Changing Input using functional component done

--- Module-4/React/functional-comp-react/src/components/Counter.js
+++ Module-4/React/functional-comp-react/src/components/Counter.js
@@ -2,15 +2,17 @@ import { useState } from "react"
 
 
  function Counter(){
-     const [count,setCount] = useState(0)
-
-     return(
-         <div>
-             <h1>This is a Counter {count}</h1>
-             <button onClick={()=>setCount(count+1)}>+</button>
-             <button onClick={()=>setCount(count-1)}>-</button>
-         </div>
-     )
+    const [count,setCount] = useState(0)
+    const [text,setText] = useState('')
+    return(
+        <div>
+            <h1>This is a Counter {count}</h1>
+            <button onClick={()=>setCount(count+1)}>+</button>
+            <button onClick={()=>setCount(count-1)}>-</button>
+            <input value={text} onChange={(e)=>setText(e.target.value)}></input>
+            <h1>{text}</h1>
+        </div>
+    )
  }
 
  export default Counter 
