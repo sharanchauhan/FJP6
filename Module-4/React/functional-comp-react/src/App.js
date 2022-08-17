f710226ded10de4987e7902d03b8297e7546b35a
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-08-17 18:45:13 +0530

    Learn't the use of context API

--- Module-4/React/functional-comp-react/src/App.js
+++ Module-4/React/functional-comp-react/src/App.js
@@ -4,10 +4,14 @@ import Counter from './components/Counter'
 import Ue1 from './components/Ue1';
 import Ue2 from './components/Ue2'
  import Ue3 from './components/Ue3';
+import Test from './components/Context/Test';
+import Parent from './components/Context/Parent';
 function App() {
   return (
     // <Counter/>
-    <Ue3/>
+    // <Ue3/>
+    <Test/>
+    // <Parent/>
   );
 }
 
